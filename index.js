f2db6c956e7d0ee4eb69bb37cb7475c4da6f4e92
Gowtham M <gowtham3032004m@gmail.com>
2024-08-09 15:58:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const todoRoutes = require('./routes/todos');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3500;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/items', todoRoutes);
+
+// MongoDB Connection
+mongoose.connect(process.env.DATABASE_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error(err));
+
+// Start Server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ routes/todos.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const Todo = require('../models/Todo');
+
+// Get all todos
+router.get('/', async (req, res) => {
+    try {
+        const todos = await Todo.find();
+        res.json(todos);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+
+// Create a new todo
+router.post('/', async (req, res) => {
+    const todo = new Todo({
+        item: req.body.item,
+    });
+
+    try {
+        const newTodo = await todo.save();
+        res.status(201).json(newTodo);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Update a todo
+router.patch('/:id', async (req, res) => {
+    try {
+        const todo = await Todo.findById(req.params.id);
+        if (req.body.item != null) {
+            todo.item = req.body.item;
+        }
+        if (req.body.checked != null) {
+            todo.checked = req.body.checked;
+        }
+        const updatedTodo = await todo.save();
+        res.json(updatedTodo);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Delete a todo
+router.delete('/:id', async (req, res) => {
+    try {
+        await Todo.findByIdAndDelete(req.params.id);
+        res.json({ message: 'Todo deleted' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+module.exports = router;
