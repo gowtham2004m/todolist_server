de81c2d47537b0e43de11bdccfd954ae479d259e
Gowtham M <gowtham3032004m@gmail.com>
2024-08-09 17:10:39 +0530

    index.js updated

--- index.js
+++ index.js
@@ -8,16 +8,21 @@ const app = express();
 const PORT = process.env.PORT || 3500;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'https://master--todolistfrontpage.netlify.app',
+}));
 app.use(express.json());
 
 // Routes
 app.use('/items', todoRoutes);
 
 // MongoDB Connection
-mongoose.connect(process.env.DATABASE_URI)
+mongoose.connect(process.env.DATABASE_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
     .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error(err));
+    .catch(err => console.error('Failed to connect to MongoDB:', err));
 
 // Start Server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
